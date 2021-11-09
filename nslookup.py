7f787ff7295f1dbe0ec7d94f602a7e71fde74479
IoanMica <92159342+ioanmica@users.noreply.github.com>
2021-11-09 10:07:30 +0200

    Update nslookup.py

--- nslookup.py
+++ nslookup.py
@@ -7,15 +7,11 @@ print(output)
 
 smth = "this is proba 2"
 
+"""
 def find_user(username):
     with connection.cursor() as cur:
         cur.execute(f"""select username from USERS where name = '%s'""" % username)
         output = cur.fetchone()
     return output
-
-def find_item(itemname):
-    with connection.cursor() as cur:
-        cur.execute(f"""select username from ITEMS where name = '%s'""" % itemname)
-        output = cur.fetchone()
-    return output
+"""
 
